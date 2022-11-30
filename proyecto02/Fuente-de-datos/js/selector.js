ba330ca4dc0e6692a2a7a4904dbc6df16b78cf41
henqchav <93171012+henqchav@users.noreply.github.com>
2022-11-30 03:07:06 -0500

    casi done

--- proyecto02/Fuente-de-datos/js/selector.js
+++ proyecto02/Fuente-de-datos/js/selector.js
@@ -56,20 +56,65 @@ let mostrarImagen = () =>{
             let fotos = pokemons.sprites.front_default
             let experiencia = pokemons.base_experience
             let abilidad1 = pokemons.abilities[0].ability.name
-            /*let abilidad2 = pokemons.abilities[1].ability.name*/
-            
+            let abilidad2 = pokemons.abilities[1] ? pokemons.abilities[1].ability.name : "no tiene abilidad 2 "
+            let id = pokemons.id    
+            let hpnum = pokemons.stats[0].base_stat
+            let atknum = pokemons.stats[1].base_stat
+            let defnum = pokemons.stats[2].base_stat
+            let satknum = pokemons.stats[3].base_stat
+            let sdefnum = pokemons.stats[4].base_stat
+            let spdnum = pokemons.stats[5].base_stat
 
             document.getElementById("imagen").innerHTML = ''
             let plantilla = `
-                    <div class="col-lg-3">
-                        <h3>${nombre}</h3>
-                        <img src = "${fotos}">
+                    
+                    <div class="">
+                        <h4>${nombre}</h4>
+                        <img src = "${fotos}" class="img-fluid">
+                        <h3>#${id}</h3>
                         <p>Experiencia base: ${experiencia}</p>
                         <p>Abilidad 1: ${abilidad1}</p>
-                        <p>Abilidad 2: ${abilidad1}</p>
+                        <p>Abilidad 2: ${abilidad2}</p>
                     </div>
                 `
-                document.getElementById("imagen").innerHTML += plantilla
+            document.getElementById("imagen").innerHTML += plantilla
+
+            let grafica_ns = document.getElementById("statsNormales").getContext("2d")
+            
+            var statsNormalesqa = new Chart(grafica_ns,{
+                type:"bar",
+                data:{
+                    labels:["vida","ataque","defensa"],
+                    datasets:[
+                        {
+                            label:"Grafica de stats normales",
+                            backgroundColor:"236, 240, 241",
+                            data:[parseInt(`${hpnum}`),parseInt(`${atknum}`),parseInt(`${defnum}`)],
+                            backgroundColor : ["rgb(17, 122, 101)","rgb(17, 122, 101)","rgb(17, 122, 101)"]
+                        }
+                    ]
+                }
+
+            })
+            let grafica_ss = document.getElementById("statsEspeciales").getContext("2d")
+            
+            var statsNormalesss = new Chart(grafica_ss,{
+                type:"polarArea",
+                data:{
+                    labels:["atake especial","defensa especial","velocidad"],
+                    datasets:[
+                        {
+                            label:"Grafica de stats especiales",
+                            backgroundColor:"236, 240, 241",
+                            data:[parseInt(`${satknum}`),parseInt(`${sdefnum}`),parseInt(`${spdnum}`)],
+                            backgroundColor : ["rgb(31, 97, 141)","rgb(52, 152, 219 )","rgb(27, 79, 114)"]
+                        }
+                    ]
+                }
+
+            })
+
+
             })
         .catch(function(error) {
             console.error("¡Error!", error);
